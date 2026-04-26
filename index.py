dbb3954372d636761ff5e46aa58333862ba6f013
Franz Llerena <franzllerenaflores@gmail.com>
2026-04-26 13:51:01 -0500

    agregando convertidor

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def inicio():
     return render_template("login.html", mensaje="")
@@ -12,19 +13,34 @@ def login():
     clave = request.form.get("clave")
 
     if usuario == "admin" and clave == "1234":
-        return f"<h1>Bienvenido, {usuario}</h1><p>Acceso correcto.</p>"
+        return f"""
+        <h1>Bienvenido, {usuario}</h1>
+        <p>Acceso correcto.</p>
+        <a href="/convertidor">Ir al convertidor</a>
+        """
     else:
         return render_template("login.html", mensaje="Usuario o contraseña incorrectos")
 
 @app.route("/predict/<float:celsius>", methods=["GET"])
 def predict_get(celsius):
-    celsius = float(celsius)
     fahrenheit = (celsius * 9/5) + 32
 
     return jsonify({
         "celsius": celsius,
         "fahrenheit": fahrenheit
     })
+
+
+@app.route("/convertidor")
+def convertidor():
+    return render_template("convertidor.html", resultado=None)
+
+@app.route("/convertir", methods=["POST"])
+def convertir():
+    celsius = float(request.form.get("celsius"))
+    fahrenheit = (celsius * 9/5) + 32
+
+    return render_template("convertidor.html", resultado=fahrenheit)
 
 
 if __name__ == "__main__":
